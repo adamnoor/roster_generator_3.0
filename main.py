0ba5da81643687c37527123cab65d28919896660
Adam Noor <adaminoor@yahoo.com>
2022-06-03 14:51:26 -0400

    minor space change

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from create_table import run_create
 path = os.path.exists('football.sqlite')
 
 if path:
-    
     print("Currently you have files in the generated_files folder")
     print("")
     print("Select 1 to rewrite the files")
@@ -13,14 +12,13 @@ if path:
     print("")
     user_input = int(input("Make a selection: "))
     if user_input == 1:
-        
         run_create()
     
 else:
-    
     run_create()
 
 path = os.path.exists('football.sqlite')
+
 if path:
     print("")
     print("We will now build a stack")
